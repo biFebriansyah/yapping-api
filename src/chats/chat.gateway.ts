416053b03c35b04776c71da694de963c90f77bfa
biFebriansyah <bukanebi@gmail.com>
2024-12-29 23:07:20 +0700

    send and recieve message

--- src/chats/chat.gateway.ts
+++ src/chats/chat.gateway.ts
@@ -1,16 +1,43 @@
 import {
   WebSocketGateway,
+  WebSocketServer,
   SubscribeMessage,
   MessageBody,
+  OnGatewayConnection,
+  ConnectedSocket,
 } from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+
+interface MessageData {
+  value: string;
+  to?: string;
+  seender: boolean;
+}
 
 @WebSocketGateway({ cors: { origin: '*' } })
-class ChatGateway {
-  @SubscribeMessage('message')
-  async chatManage(@MessageBody() data: string): Promise<string> {
+class ChatGateway implements OnGatewayConnection {
+  @WebSocketServer()
+  private server: Server;
+
+  async handleConnection(socket: Socket) {
     try {
-      console.log(data);
-      return data;
+      console.log(`connected clientId: ${socket.id}`);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @SubscribeMessage('message')
+  async handleMessage(
+    @MessageBody() msg: MessageData,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    try {
+      if (!msg.to) {
+        socket.broadcast.emit('receive-msg', msg.value);
+      } else {
+        this.server.to(msg.to).emit('receive-msg', msg.value);
+      }
     } catch (error) {
       throw error;
     }
